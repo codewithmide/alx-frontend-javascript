236f5b86758ea0c91d1ea8b706be90075f7f19c7
Olumide Micheal <olumidemicheal7@gmail.com>
2022-12-20 11:38:18 +0100

    test

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,13 +1,13 @@
-export function taskFirst() {
+export function taskFirst () {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-export function getLast() {
+export function getLast () {
   return ' is okay';
 }
 
-export function taskNext() {
+export function taskNext () {
   let combination = 'But sometimes let';
   combination += getLast();
 
