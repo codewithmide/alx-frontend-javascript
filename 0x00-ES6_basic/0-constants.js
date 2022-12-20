258aa044a8711c1fc327a18116773364c70f2528
Olumide Micheal <olumidemicheal7@gmail.com>
2022-12-20 18:53:45 +0100

    used airbnb style guide

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,12 +1,12 @@
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
+  const task = 'I prefer const when I can.';
+  return task;
 }
 export function getLast() {
-    return ' is okay';
+  return ' is okay';
 }
 export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-    return combination;
+  let combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
 }
