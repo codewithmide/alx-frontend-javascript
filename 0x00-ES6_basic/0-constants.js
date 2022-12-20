0a5c98baf93eaec745d75f59e88643ca90a19720
Olumide Micheal <olumidemicheal7@gmail.com>
2022-12-20 18:20:29 +0100

    removed default

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,12 @@
-export default function taskFirst() {
+export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
 }
-
-export default function getLast() {
+export function getLast() {
     return ' is okay';
 }
-
-export default function taskNext() {
+export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
-    
     return combination;
 }
