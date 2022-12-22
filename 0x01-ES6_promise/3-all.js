037b9246cfed2875a4a3dc72a5d7ba008b783147
Olumide Micheal <olumidemicheal7@gmail.com>
2022-12-22 12:01:10 +0100

    promiseAll

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
-  return Promise.all([photo, user])
-    .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`)
-      .catch('Signup system offline'));
+  return Promise.all([photo, user]).then(
+    (data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
+    () => console.log('Signup system offline'),
+  );
 }
