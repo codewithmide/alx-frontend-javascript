8362258279b4a9a90a768ffa3678b62ff2934cf9
Olumide Micheal <olumidemicheal7@gmail.com>
2022-12-23 18:14:59 +0100

    Await / Async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
