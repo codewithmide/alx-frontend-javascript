b228ada289fb1ac7a09fb1b2539b46936fda2c73
Olumide Micheal <olumidemicheal7@gmail.com>
2022-12-24 09:46:31 +0100

    used the toPrimitive function

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,28 +1,28 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        this._size = size;
-        this._location = location;
-    }
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
 
-    [Symbol.toPrimitive](dataType) {
-        if (dataType === 'string') return this._location;
-        if (dataType === 'number') return this._size;
-        return (this._location, this._size);
-      }
+  [Symbol.toPrimitive](dataType) {
+    if (dataType === 'string') return this._location;
+    if (dataType === 'number') return this._size;
+    return (this._location, this._size);
+  }
 
-      set size(value) {
-        this._size = value;
-      }
-    
-      set location(value) {
-        this._location = value;
-      }
+  set size(value) {
+    this._size = value;
+  }
 
-      get size() {
-        return this._size;
-      }
-    
-      get location() {
-        return this._location;
-      }
+  set location(value) {
+    this._location = value;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
 }
