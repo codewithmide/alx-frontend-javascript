73d7b3ec0f6d32d8ea7c9d6620a2050bb34802de
Olumide Micheal <olumidemicheal7@gmail.com>
2023-01-02 11:02:01 +0100

    a simple array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  const arr = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return arr;
+}
