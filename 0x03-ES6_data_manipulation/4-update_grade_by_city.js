ee07d67d0de411ba4a16e0637c686a5a03306653
Olumide Micheal <olumidemicheal7@gmail.com>
2023-01-03 17:37:18 +0100

    ignored node module

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,6 +3,13 @@
 
 // It should accept a list of students (from getListStudents), a city (String) and newGrades (Array of “grade” objects) as parameters.
 // If a student doesn’t have grade in newGrades, the final grade should be N/A.
-export default function updateStudentGradeByCity(students, city, newGrades){
-    
+/* eslint-disable */
+export default function updateStudentGradeByCity(students, city, newGrade){
+    return students
+      .filter((student) => student.location == city)
+      .map((student) => {
+        let grades = newGrade.find((grade) => grade.studentId == student.id);
+        let grade = grades ? grades.grade : "N/A";
+        return { ...student, grade };
+       });
 }
