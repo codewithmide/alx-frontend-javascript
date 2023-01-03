26b44d137c18a69ed56317eb52ccd4e03f8da5c3
Olumide Micheal <olumidemicheal7@gmail.com>
2023-01-03 20:39:19 +0100

    initialized an array of set

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length); // ArrayBuffer
   const dataView = new DataView(buffer); // DataView
-  if (position >= length) throw new RangeError('Position outside range');
+  if (position >= length) throw new RangeError('Position outside range'); // Throws RangeError if it exceeds length
   dataView.setUint8(position, value);
   return dataView;
 }
