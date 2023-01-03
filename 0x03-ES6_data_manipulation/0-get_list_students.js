9eb85061d67af68e59fff54993c26aab948f76fd
Olumide Micheal <olumidemicheal7@gmail.com>
2023-01-03 10:14:23 +0100

    return directly

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,7 @@
 export default function getListStudents() {
-  const arr = [
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-
-  return arr;
 }
---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,8 @@
+// Create a function updateStudentGradeByCity
+// that returns an array of students for a specific city with their new grade
+
+// It should accept a list of students (from getListStudents), a city (String) and newGrades (Array of “grade” objects) as parameters.
+// If a student doesn’t have grade in newGrades, the final grade should be N/A.
+export default function updateStudentGradeByCity(students, city, newGrades){
+    
+}
