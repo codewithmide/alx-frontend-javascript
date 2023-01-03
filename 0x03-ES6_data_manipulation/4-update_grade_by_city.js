e8608e77ad6cb7ad5fe952c5e0bd40bf7fc276c9
Olumide Micheal <olumidemicheal7@gmail.com>
2023-01-03 17:41:01 +0100

    combined filter and sort

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,15 @@
-// Create a function updateStudentGradeByCity
-// that returns an array of students for a specific city with their new grade
+/** Create a function updateStudentGradeByCity
+that returns an array of students for a specific city with their new grade
 
-// It should accept a list of students (from getListStudents), a city (String) and newGrades (Array of “grade” objects) as parameters.
-// If a student doesn’t have grade in newGrades, the final grade should be N/A.
-/* eslint-disable */
-export default function updateStudentGradeByCity(students, city, newGrade){
-    return students
-      .filter((student) => student.location == city)
-      .map((student) => {
-        let grades = newGrade.find((grade) => grade.studentId == student.id);
-        let grade = grades ? grades.grade : "N/A";
-        return { ...student, grade };
-       });
+It should accept a list of students (from getListStudents), a city (String)
+and newGrades (Array of “grade” objects) as parameters.
+If a student doesn’t have grade in newGrades, the final grade should be N/A.* */
+export default function updateStudentGradeByCity(students, city, newGrade) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grades = newGrade.find((grade) => grade.studentId === student.id);
+      const grade = grades ? grades.grade : 'N/A';
+      return { ...student, grade };
+    });
 }
