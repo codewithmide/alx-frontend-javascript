3f3f8c72a3d3770bd531605f0f3596c3a77e007e
Olumide Micheal <olumidemicheal7@gmail.com>
2023-01-04 20:46:13 +0100

    new types and function:

---
+++ 0x04-TypeScript/task_2/js/main.js
@@ -0,0 +1,39 @@
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return 'Working from home';
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return 'Getting a coffee break';
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return 'Getting to director tasks';
+    };
+    return Director;
+}());
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return 'Cannot work from home';
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return 'Cannot have a break';
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return 'Getting to work';
+    };
+    return Teacher;
+}());
+function createEmployee(salary) {
+    if (typeof salary === 'number' && salary < 500) {
+        return Teacher;
+    }
+    else {
+        return Director;
+    }
+}
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
